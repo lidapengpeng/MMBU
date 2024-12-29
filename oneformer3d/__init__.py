4f300e4ef9d11cb16fb7e41a1c1e1557ade343fd
lidapengpeng <li18838956649@gmail.com>
2024-12-29 01:56:28 +0000

    Initial commit: project structure

---
+++ oneformer3d/__init__.py
@@ -0,0 +1,23 @@
+from .models.oneformer3d import (
+    ScanNetOneFormer3D, ScanNet200OneFormer3D, S3DISOneFormer3D,
+    InstanceOnlyOneFormer3D)
+from .models.spconv_unet import SpConvUNet
+from .models.mink_unet import Res16UNet34C
+from .models.query_decoder import ScanNetQueryDecoder, QueryDecoder
+from .loss.unified_criterion import (
+    ScanNetUnifiedCriterion, S3DISUnifiedCriterion)
+from .loss.semantic_criterion import (
+    ScanNetSemanticCriterion, S3DISSemanticCriterion)
+from .loss.instance_criterion import (
+    InstanceCriterion, QueryClassificationCost, MaskBCECost, MaskDiceCost,
+    HungarianMatcher, SparseMatcher, OneDataCriterion)
+from .data_processing.loading import LoadAnnotations3D_, NormalizePointsColor_
+from .data_processing.formatting import Pack3DDetInputs_
+from .data_processing.transforms_3d import (
+    ElasticTransfrom, AddSuperPointAnnotations, SwapChairAndFloor, PointSample_)
+from .data_processing.data_preprocessor import Det3DDataPreprocessor_
+from .metrics.unified_metric import UnifiedSegMetric
+from .data_processing.scannet_dataset import ScanNetSegDataset_, ScanNet200SegDataset_
+from .data_processing.s3dis_dataset import S3DISSegDataset_
+from .data_processing.structured3d_dataset import Structured3DSegDataset, ConcatDataset_
+from .data_processing.structures import InstanceData_
